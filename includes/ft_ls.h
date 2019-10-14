f2140278fb6e19e25fcdd68ef2100693e50b018d
Merkert Plutarch <mplutarc@vo-c2.21-school.ru>
2019-10-14 17:18:09 +0300

    looks like ls -a but twice

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,12 +6,25 @@
 /*   By: mplutarc <mplutarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 20:23:13 by mplutarc          #+#    #+#             */
-/*   Updated: 2019/10/08 20:35:39 by mplutarc         ###   ########.fr       */
+/*   Updated: 2019/10/14 16:41:50 by mplutarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS
 # define FT_LS
+
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <pwd.h>
+# include <grp.h>
+# include <dirent.h>
+# include <stdlib.h>
+
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
 
 int     main();
 
--- src/main.c
+++ src/main.c
@@ -6,11 +6,36 @@
 /*   By: mplutarc <mplutarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 20:23:06 by mplutarc          #+#    #+#             */
-/*   Updated: 2019/10/08 20:35:31 by mplutarc         ###   ########.fr       */
+/*   Updated: 2019/10/14 17:17:18 by mplutarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int     main()
+#include "../includes/ft_ls.h"
+
+unsigned directory(char *theDir)
 {
+    DIR		*dir;
+    struct	dirent *entry;
+	int		i = 0;
+
+	dir = opendir(theDir); //открытие директории
+	while((entry = readdir(dir)))  //пока директория читаема, но почему-то оно прочитывает его по второму кругу, разберёшься, почему?
+	{
+		i++;
+		if (!dir)
+		{
+        	printf( "Error opening %s: %s", theDir, strerror(errno));
+        	return 0;
+    	}
+		printf("i = %d\t %s\n", i, entry->d_name);
+		// printf("d_ino: %llu\t name: %s\t type: [%d]\t reclen: %d\n", entry->d_ino, entry->d_name, entry->d_type, entry->d_reclen);
+    }
+	closedir(dir);
     return (0);
+}
+
+int		main()
+{
+	directory(".");
+	return (0);
 }
